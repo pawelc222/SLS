12f61bf16799cdccdfc1ca9c1a1303f0aba530be
pawelc222 <pawelc222@gmail.com>
2014-10-26 21:56:38 +0100

    Self Host

---
+++ SLS.WCFService.SelfHost/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace SLS.WCFService.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ServiceHost svcHost = null;
+            try
+            {
+                svcHost = new ServiceHost(typeof(SLS.WCFService.Service1));
+                svcHost.Open(); Console.WriteLine("\n\nService is Running  at following address");
+                Console.WriteLine("\nhttp://localhost:12345/SLSMobile");
+                Console.WriteLine("\nnet.tcp://localhost:45678/SLS");
+            }
+            catch (Exception eX)
+            {
+                svcHost = null;
+                Console.WriteLine("Service can not be started \n\nError Message [" + eX.Message + "]");
+                Console.ReadKey();
+            } if (svcHost != null)
+            {
+                Console.WriteLine("\nPress any key to close the Service");
+                Console.ReadKey();
+                svcHost.Close();
+                svcHost = null;
+            }            
+        }
+    }
+}
